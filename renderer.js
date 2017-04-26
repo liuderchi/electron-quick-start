81cc79d6ad53a217f1d471280e4594fe668fb044
liuderchi <liuderchi@gmail.com>
2017-04-26 22:16:18 +0800

    lec 6: build native node package using electron
    - add .electron-npm with specified platform info

--- renderer.js
+++ renderer.js
@@ -3,3 +3,14 @@
 // All of the Node.js APIs are available in this process.
 
 console.log('renderer.js is loading')
+
+// NOTE testing running node package bcrypt in Chromium
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+var salt = bcrypt.genSaltSync(saltRounds);
+var hash = bcrypt.hashSync(myPlaintextPassword, salt);
+
+console.log('hash using bscript', hash);
