4c15e3fd13d4b570ae6d0e00c2bf6c16666a36ea
liuderchi <liuderchi@gmail.com>
2017-08-08 23:31:47 +0800

    lec 39: add modal template in main.html, click handler in app.js

--- renderer/app.js
+++ renderer/app.js
@@ -1,3 +1,7 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// add btn click handlers
+$('.open-add-modal').click(() => {
+  $('#add-modal').addClass('is-active')
+})
+$('.close-add-modal').click(() => {
+  $('#add-modal').removeClass('is-active')
+})
