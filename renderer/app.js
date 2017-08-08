7af2533914a587ef95a87c8929364d59f5501430
liuderchi <liuderchi@gmail.com>
2017-08-09 00:06:25 +0800

    lec 39: poc of handle submit event, with enter key press listener

--- renderer/app.js
+++ renderer/app.js
@@ -5,3 +5,21 @@ $('.open-add-modal').click(() => {
 $('.close-add-modal').click(() => {
   $('#add-modal').removeClass('is-active')
 })
+
+// add item submit handler
+$('#add-button').click(() => {
+  // Get URL from input
+  let newItemURL = $('#item-input').val()
+
+  if (newItemURL) {
+
+    console.log(newItemURL)
+
+  }
+})
+
+$('#item-input').keyup((event) => {
+  if (event.key === 'Enter') {
+    $('#add-button').click()
+  }
+})
